6d7f0f5d9ed1aa31e030371bb4b71757f1870ffa
Manaswini1869 <goginenim18069@gmail.com>
2024-12-11 00:19:11 -0500

    feat(chatapp): added models

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const jwt = require("jsonwebtoken");
+const cors = require('cors');
+
+dotenv.config()
+mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWT_SECRET;
+
 const app = express();
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}));
 
 app.get('', (req, res) => {
     res.json('Hello World! Bye World!');
@@ -9,8 +24,18 @@ app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
-app.post('/register', (req, res) => {
-
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const createdUser = await User.create({username, password});
+        jwt.sign({userId: createdUser._id, username}, jwtSecret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).status(201).json('ok');
+        });
+    } catch(err) {
+        if (err) throw err;
+        res.status(500).json('error');
+    }
 })
 
 app.listen(4040);
--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import axios from "axios";
 
 export default function Register() {
     const [username, setUser] = useState("");
     const [password, setPassword] = useState("");
+    async function register(ev) {
+        ev.preventDefault();
+        await axios.post('/register', {username, password})
+    }
     return (
         <div className="bg-blue-50 h-screen flex items-center">
-            <form className="w-64 mx-auto mb-12">
+            <form className="w-64 mx-auto mb-12" onSubmit={register}>
                 <input value={username} onChange={ev => setUser(ev.target.value)} type="text" placeholder="Username" className="block w-full rounded-sm p-2 mb-2 border"></input>
                 <input value={password} onChange={ev => setPassword(ev.target.value)} type="password" placeholder="Password" className="block w-full rounded-sm p-2 mb-2 border"></input>
                 <button className="bg-blue-500 text-white block w-full rounded-sm p-2">Register</button>
