e86edd1c10c5db37d7782f8f91c8e1758953e722
Manaswini1869 <goginenim18069@gmail.com>
2024-12-16 18:31:40 -0500

    feat(chatapp): final demo app

--- api/index.js
+++ api/index.js
@@ -4,21 +4,33 @@ const mongoose = require("mongoose");
 const User = require("./models/User");
 const jwt = require("jsonwebtoken");
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
-dotenv.config()
-mongoose.connect(process.env.MONGO_URL);
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connected to MongoDB successfully");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+});
+
 const jwtSecret = process.env.JWT_SECRET;
-
+const bcryptSalt = bcrypt.genSaltSync(10);
 const app = express();
 app.use(express.json());
+
+// Fix CORS config: Remove the leading space in the origin URL
 app.use(cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
+    credentials: true, // Allow cookies to be sent
+    origin: "http://localhost:5173", // Remove space before URL
 }));
 
 app.get('', (req, res) => {
     res.json('Hello World! Bye World!');
-})
+});
 
 app.get('/test', (req, res) => {
     res.json('test ok');
@@ -27,15 +39,21 @@ app.get('/test', (req, res) => {
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     try {
-        const createdUser = await User.create({username, password});
-        jwt.sign({userId: createdUser._id, username}, jwtSecret, {}, (err, token) => {
-            if (err) throw err;
-            res.cookie('token', token).status(201).json('ok');
+        const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+        const createdUser = await User.create({
+            username: username,
+            password: hashedPassword,
         });
-    } catch(err) {
-        if (err) throw err;
+
+        jwt.sign({ userId: createdUser._id, username }, jwtSecret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token, { sameSite: 'None', secure: true }).status(201).json({
+                id: createdUser._id,
+            });
+        });
+    } catch (err) {
         res.status(500).json('error');
     }
-})
+});
 
-app.listen(4040);
+app.listen(4040, () => console.log("Server running on http://localhost:4040"));
--- api/models/User.js
+++ api/models/User.js
@@ -1,7 +1,8 @@
-const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema ({
-    username: {type: String, unique: true},
-    password: String,
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {type:String, unique:true},
+  password: String,
 }, {timestamps: true});
 
 const UserModel = mongoose.model('User', UserSchema);
